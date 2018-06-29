1b266303dea4009937147196eddefbefe2697dd4
Brian Downing <bdowning@lavos.net>
2018-06-29 11:30:29 -0500

    Don't split windows horizontally
    Holy crap it's annoying.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -69,6 +69,9 @@
 ;;       blink-cursor-mode nil)
 (blink-cursor-mode 0)
 
+;; Don't split horizontally
+(setq split-width-threshold 9000)
+
 ;; M-x shell is a nice shell interface to use, let's make it colorful.  If
 ;; you need a terminal emulator rather than just a shell, consider M-x term
 ;; instead.
