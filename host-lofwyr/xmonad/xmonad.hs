a363d1f47899b7f7b30f4debf96291a90e794e05
Brian Downing <bdowning@lavos.net>
2023-03-24 00:07:44 -0500

    New machine

---
+++ host-lofwyr/xmonad/xmonad.hs
@@ -0,0 +1,86 @@
+import XMonad
+import XMonad.Actions.CycleWS
+import XMonad.Actions.UpdatePointer
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.InsertPosition
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.NoBorders
+import XMonad.Layout.ThreeColumns
+import XMonad.Util.Cursor
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Prompt
+import XMonad.Prompt.Shell
+import XMonad.Prompt.XMonad
+--import MyEwmhDesktops
+import XMonad.Hooks.EwmhDesktops
+import Graphics.X11.ExtraTypes.XF86
+import System.IO
+import qualified Data.Map as M
+import Control.Monad
+import Data.Maybe
+
+myManageHook = composeAll 
+  [ className =? "Squeak" --> doFloat
+  , className =? "Plugin-container" --> doFloat
+  , title =? "TIS-100" --> doIgnore
+  , isDialog --> doCenterFloat
+  , isFullscreen --> doFullFloat
+  ]
+
+layout = tiled ||| three ||| noBorders Full
+  where
+     tiled   = Tall nmaster delta (1/2)
+     three   = ThreeCol nmaster delta (1/3)
+     nmaster = 1
+     delta   = 3/100
+
+main = do
+  --xmproc <- spawnPipe "xmobar"
+  xmonad $ ewmhFullscreen $ ewmh $ def {
+    terminal = "urxvt",
+    manageHook = myManageHook <+> manageHook def,
+    -- <+> insertPosition Master Newer 
+    layoutHook = smartBorders $ layout,
+    --logHook = dynamicLogWithPP xmobarPP {
+    --  ppOutput = hPutStrLn xmproc,
+    --  ppTitle = xmobarColor "green" ""
+    --  } <+> updatePointer (TowardsCentre 0.025 0.025),
+    --logHook = updatePointer (TowardsCentre 0.025 0.025),
+    borderWidth        = 2,
+    normalBorderColor  = "#383838",
+    focusedBorderColor = "#6F6F6F",
+    keys = myKeys <+> keys def,
+    modMask = mod4Mask,
+    startupHook = do
+        setDefaultCursor xC_left_ptr
+    }
+
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList [
+  -- ((modm, xK_F12   ), xmonadPrompt defaultXPConfig),
+  -- ((modm, xK_r     ), shellPrompt  defaultXPConfig),
+  ((modm, xK_s     ), spawn "maim -s -u | xclip -selection clipboard -t image/png -i"),
+  ((modm .|. shiftMask, xK_s), spawn "maim -u | xclip -selection clipboard -t image/png -i"),
+  ((modm, xK_Escape), toggleWS),
+  ((0, xF86XK_AudioMute), spawn "amixer -q set Master toggle"),
+  ((0, xF86XK_AudioRaiseVolume), spawn "amixer -q set Master 5%+ unmute"),
+  ((0, xF86XK_AudioLowerVolume), spawn "amixer -q set Master 5%- unmute"),
+  ((0, xF86XK_MonBrightnessUp), spawn "$HOME/bin/backlight-adjust 5"),
+  ((0, xF86XK_MonBrightnessDown), spawn "$HOME/bin/backlight-adjust -5")
+  ]
+
+addNETSupported :: Atom -> X ()
+addNETSupported x = withDisplay $ \dpy -> do
+  r               <- asks theRoot
+  a_NET_SUPPORTED <- getAtom "_NET_SUPPORTED"
+  a               <- getAtom "ATOM"
+  liftIO $ do
+    sup <- (join . maybeToList) <$> getWindowProperty32 dpy a_NET_SUPPORTED r
+    when (fromIntegral x `notElem` sup) $
+      changeProperty32 dpy r a_NET_SUPPORTED a propModeAppend [fromIntegral x]
+
+addEWMHFullscreen :: X ()
+addEWMHFullscreen = do
+  wms <- getAtom "_NET_WM_STATE"
+  wfs <- getAtom "_NET_WM_STATE_FULLSCREEN"
+  mapM_ addNETSupported [wms, wfs]
